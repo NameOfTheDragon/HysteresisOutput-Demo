fe2bcb15da3f7b33d9c1d53de208f51ac00815ba
Tim Long <Tim@tigranetworks.co.uk>
2021-09-08 00:50:47 +0100

    Initial version

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
 #include <Arduino.h>
+#include <Timer.h>
+#include <HysteresisOutput.h>
 
-void setup() {
-  // put your setup code here, to run once:
+constexpr Duration minimumOnTime = Timer::Seconds(5);
+constexpr Duration minimumOffTime = Timer::Seconds(10);
+
+bool inputState;
+
+void stateChanger(bool state)
+{
+  Serial.print("State changed to ");
+  Serial.println(state);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// The folowing definition contains a lambda expression (advanced C++)
+HysteresisOutput demo(minimumOnTime, minimumOffTime, [](bool state)
+                      { stateChanger(state); });
+
+Timer simulateRandomInput;
+
+void setup()
+{
+  simulateRandomInput.setDuration(Timer::Seconds(1));
+}
+
+void loop()
+{
+  demo.loop();
+  if (simulateRandomInput.expired())
+  {
+    Duration randomDuration = Timer::Milliseconds((int)random(1000, 20000));
+    simulateRandomInput.setDuration(randomDuration);
+    inputState = !inputState;
+    Serial.print("Input state -> ");
+    Serial.println(inputState);
+    demo.setInputState(inputState);
+  }
 }
